4056d8f1c3f45e29b8f87bd42d7256d49c77a92a
wpblank <pyr15095825383@live.com>
2019-03-30 13:55:27 +0800

    一个开始

---
+++ src/main/java/cn/lzumi/myfavoriteanime/MyfavoriteanimeApplication.java
@@ -0,0 +1,13 @@
+package cn.lzumi.myfavoriteanime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfavoriteanimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyfavoriteanimeApplication.class, args);
+    }
+
+}
